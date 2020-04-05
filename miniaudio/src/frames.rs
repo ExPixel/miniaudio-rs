edc4b0a2438c2d13952c6a3b07ddb9499b42e5e4
Adolph C <adolphc@outlook.com>
2020-04-05 06:44:18 -0400

    Added functions for iterating over frames and samples.

--- miniaudio/src/frames.rs
+++ miniaudio/src/frames.rs
@@ -8,6 +8,7 @@ pub struct Frames<'s> {
 }
 
 impl<'s> Frames<'s> {
+    #[inline]
     pub fn wrap(data: &'s [u8], format: Format, channels: u32) -> Frames<'s> {
         Frames {
             data,
@@ -18,6 +19,7 @@ impl<'s> Frames<'s> {
 
     /// Convert this frames samples into another format, placing the new converted
     /// frames into `dest`.
+    #[inline]
     pub fn convert(&self, dest: &FramesMut, dither_mode: DitherMode) {
         assert!(
             self.frame_count() == dest.frame_count(),
@@ -41,28 +43,56 @@ impl<'s> Frames<'s> {
         self.data.as_ptr()
     }
 
-    pub fn data(&self) -> &[u8] {
+    #[inline]
+    pub fn as_bytes(&self) -> &[u8] {
         self.data
     }
 
+    #[inline]
+    pub fn as_samples<'t, S: Sample>(&'t self) -> &'t [S] {
+        assert!(
+            self.format() == S::format(),
+            "format mismatch (frames: {:?}, requested: {:?})",
+            self.format,
+            S::format()
+        );
+
+        let len = self.sample_count();
+        unsafe { std::slice::from_raw_parts(self.data.as_ptr().cast::<S>(), len) }
+    }
+
+    #[inline]
+    pub fn frames<'t, S: 'static + Sample>(&'t self) -> impl 't + Iterator<Item = &[S]> {
+        FramesIter {
+            samples: self.as_samples(),
+            channels: self.channels,
+            offset: 0,
+        }
+    }
+
+    #[inline]
     pub fn byte_count(&self) -> usize {
         self.data.len()
     }
 
     /// Returns the number of frames contained.
+    #[inline]
     pub fn frame_count(&self) -> usize {
         self.sample_count() / self.channels as usize
     }
 
     /// Returns the number of samples contained.
+    #[inline]
     pub fn sample_count(&self) -> usize {
         self.data.len() / self.format.size_in_bytes()
     }
 
+    #[inline]
     pub fn format(&self) -> Format {
         self.format
     }
 
+    #[inline]
     pub fn channels(&self) -> u32 {
         self.channels
     }
@@ -75,6 +105,7 @@ pub struct FramesMut<'s> {
 }
 
 impl<'s> FramesMut<'s> {
+    #[inline]
     pub fn wrap(data: &'s mut [u8], format: Format, channels: u32) -> FramesMut<'s> {
         FramesMut {
             data,
@@ -93,6 +124,7 @@ impl<'s> FramesMut<'s> {
 
     /// Convert this frames samples into another format, placing the new converted
     /// frames into `dest`.
+    #[inline]
     pub fn convert(&self, dest: &FramesMut, dither_mode: DitherMode) {
         assert!(
             self.frame_count() == dest.frame_count(),
@@ -112,33 +144,176 @@ impl<'s> FramesMut<'s> {
         }
     }
 
+    #[inline]
+    pub fn as_samples<'t, S: Sample>(&'t self) -> &'t [S] {
+        assert!(
+            self.format() == S::format(),
+            "format mismatch (frames: {:?}, requested: {:?})",
+            self.format,
+            S::format()
+        );
+
+        let len = self.sample_count();
+        unsafe { std::slice::from_raw_parts(self.data.as_ptr().cast::<S>(), len) }
+    }
+
+    #[inline]
+    pub fn as_samples_mut<'t, S: Sample>(&'t mut self) -> &'t mut [S] {
+        assert!(
+            self.format() == S::format(),
+            "format mismatch (frames: {:?}, requested: {:?})",
+            self.format,
+            S::format()
+        );
+
+        let len = self.sample_count();
+        unsafe { std::slice::from_raw_parts_mut(self.data.as_mut_ptr().cast::<S>(), len) }
+    }
+
+    #[inline]
+    pub fn frames<'t, S: 'static + Sample>(&'t self) -> impl 't + Iterator<Item = &[S]> {
+        FramesIter {
+            samples: self.as_samples(),
+            channels: self.channels,
+            offset: 0,
+        }
+    }
+
+    #[inline]
+    pub fn frames_mut<'t, S: 'static + Sample>(
+        &'t mut self,
+    ) -> impl 't + Iterator<Item = &mut [S]> {
+        let channels = self.channels;
+        let samples = self.as_samples_mut();
+        let samples_len = samples.len();
+
+        FramesIterMut {
+            samples_ptr: samples.as_mut_ptr(),
+            len: samples_len,
+            channels: channels,
+            offset: 0,
+            phantom: std::marker::PhantomData,
+        }
+    }
+
+    #[inline]
     pub fn data(&self) -> &[u8] {
         self.data
     }
 
+    #[inline]
     pub fn data_mut(&mut self) -> &mut [u8] {
         self.data
     }
 
+    #[inline]
     pub fn byte_count(&self) -> usize {
         self.data.len()
     }
 
     /// Returns the number of frames contained.
+    #[inline]
     pub fn frame_count(&self) -> usize {
         self.sample_count() / self.channels as usize
     }
 
     /// Returns the number of samples contained.
+    #[inline]
     pub fn sample_count(&self) -> usize {
         self.data.len() / self.format.size_in_bytes()
     }
 
+    #[inline]
     pub fn format(&self) -> Format {
         self.format
     }
 
+    #[inline]
     pub fn channels(&self) -> u32 {
         self.channels
+    }
+}
+
+pub struct FramesIter<'s, S: Sample> {
+    samples: &'s [S],
+    channels: u32,
+    offset: usize,
+}
+
+impl<'s, S: Sample> Iterator for FramesIter<'s, S> {
+    type Item = &'s [S];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.offset >= (self.samples.len() - self.channels as usize) {
+            None
+        } else {
+            let ret = Some(&self.samples[self.offset..(self.offset + self.channels as usize)]);
+            self.offset += self.channels as usize;
+            ret
+        }
+    }
+}
+
+pub struct FramesIterMut<'s, S: Sample> {
+    samples_ptr: *mut S,
+    len: usize,
+    channels: u32,
+    offset: usize,
+    phantom: std::marker::PhantomData<&'s S>,
+}
+
+impl<'s, S: Sample> Iterator for FramesIterMut<'s, S> {
+    type Item = &'s mut [S];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.offset >= (self.len - self.channels as usize) {
+            None
+        } else {
+            // FIXME The compiler doesn't like me doing the same thing that I do in FramesIter in here
+            // using mut and gives me some cryptic error, so I'm just using pointers for now.
+            let ret = Some(unsafe {
+                std::slice::from_raw_parts_mut(
+                    self.samples_ptr.add(self.offset),
+                    self.channels as usize,
+                )
+            });
+            self.offset += self.channels as usize;
+            ret
+        }
+    }
+}
+
+pub trait Sample {
+    /// Returns the format of this sample.
+    fn format() -> Format;
+}
+
+impl Sample for u8 {
+    fn format() -> Format {
+        Format::U8
+    }
+}
+
+impl Sample for i16 {
+    fn format() -> Format {
+        Format::S16
+    }
+}
+
+impl Sample for [u8; 3] {
+    fn format() -> Format {
+        Format::S24
+    }
+}
+
+impl Sample for i32 {
+    fn format() -> Format {
+        Format::S32
+    }
+}
+
+impl Sample for f32 {
+    fn format() -> Format {
+        Format::F32
     }
 }
